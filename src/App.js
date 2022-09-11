02e562cc3321868f4a6b6d9f9919bf5be48edf8a
Aranthiël <nmoronidalmasso@gmail.com>
2022-09-11 14:15:06 -0300

    se agrega configuracion para deployar con GitHub Pages

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Home from './pages/home';
 import Game from './pages/game';
 import About from './pages/about';
 import 'bulma/css/bulma.min.css';
+import packageInfo from "../package.json";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={getBasename}>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/game' element={<Game/>}/>
@@ -15,6 +16,11 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
+}
+
+function getBasename(){
+  let array = packageInfo.homepage.split("/");
+  return array[array.length-1];
 }
 
 export default App;
